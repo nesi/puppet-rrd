5f7c76e72a7bbd200f31a146c6706a2948dc534d
Aaron Hicks <hicksa@landcareresearch.co.nz>
2013-08-14 16:31:57 +1200

    update cache spec test

--- spec/classes/rrd/cache_spec.rb
+++ spec/classes/rrd/cache_spec.rb
@@ -9,5 +9,12 @@ describe 'rrd::cache', :type => :class do
     end
     it {should include_class('rrd::params')}
     it {should contain_package('rrdcached')}
+    it {should contain_service('rrdcached')}
+    it {should contain_file('rrdcached.conf').with(
+      'path'  => '/etc/default/rrdcached',
+      )
+    }
+    it { should contain_file('rrdcached.conf').with_content %r{^DISABLE=0$} }
+    it { should contain_file('rrdcached.conf').with_content %r{^ENABLE_COREFILES=0$} }
   end
 end
