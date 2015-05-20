f18ac428e993a3ef7e8b8895a1e661fe6341c7e7
Aaron Hicks <hicksa@landcareresearch.co.nz>
2015-05-20 14:04:13 +1200

    Updating rrd class tests

--- spec/classes/rrd_spec.rb
+++ spec/classes/rrd_spec.rb
@@ -1,13 +1,29 @@
 require 'spec_helper'
 
 describe 'rrd', :type => :class do
-  context "on a Debian OS" do
-    let :facts do
-      {
-        :osfamily               => 'Debian',
-      }
+
+  $supported_os.each do | os_expects |
+    os      = os_expects[:os]
+    facts   = os_expects[:facts]
+    expects = os_expects[:expects]
+    context "on #{os}" do
+      let (:facts) { facts }
+      describe "with no parameters" do
+        it { should contain_class('rrd::params') }
+        if expects[:lib_package]
+          Array(expects[:lib_package]).each do | package |
+            it { should contain_package(package).with_ensure('installed') }
+          end
+        end
+      end
     end
-    it {should contain_class('rrd::params')}
-    it {should contain_package('librrd4')}
+  end
+  context "on and Unknown operating system" do
+    let (:facts) do
+      { :osfamily => 'Unknown' }
+    end
+    it { should raise_error(Puppet::Error,
+      %r{The OS family Unknown is not supported by the rrd module.}
+    ) }
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -30,16 +30,43 @@ end
 $supported_os = on_supported_os.map do |os, facts|
   os_expects = {}
   expects = {
-
+    :cache_listen           => 'unix:/var/run/rrdcached.sock',
+    :cache_journal_dir      => '/var/lib/rrdcached/journal/',
+    :cache_timeout          => '1800',
+    :cache_delay            => '1800',
+    :cache_write_threads    => '4',
+    :cache_jump_dir         => '/var/lib/rrdcached/db/',
+    :cache_always_flush     => true,
+    :cache_enable_corefiles => false,
+    :cache_restrict_writes  => false,
+    :cache_maxwait          => '30'
   }
   case facts[:osfamily]
   when 'Debian'
     expects.merge!( {
-
+      :lib_package      => 'librrd4',
+      :tool_package     => 'rrdtool',
+      :cache_package    => 'rrdcached',
+      :ruby_packages    => ['librrd-ruby'],
+      :python_packages  => ['python-pyrrd','python-rrdtool'],
+      :perl_packages    => ['librrds-perl','librrdp-perl','librrdtool-oo-perl'],
+      :tcl_packages     => ['rrdtool-tcl'],
+      :php_packages     => ['php5-rrd'],
+      :cache_conf_file  => '/etc/default/rrdcached',
+      :cache_service    => 'rrdcached'
     } )
   when 'RedHat'
     expects.merge!( {
-
+      :lib_package      => false,
+      :tool_package     => 'rrdtool',
+      :cache_package    => 'rrdcached',
+      :ruby_packages    => 'rrdtool-ruby',
+      :python_packages  => 'rrdtool-python',
+      :perl_packages    => 'rrdtool-perl',
+      :tcl_packages     => 'rrdtool-tcl',
+      :php_packages     => 'rrdtool-php',
+      :cache_conf_file  => '/etc/default/rrdcached',
+      :cache_service    => 'rrdcached'
     } )
   end
 
